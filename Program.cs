3d371f66dcf4d4578fd1c7defb892c0209b39947
Jorge-Rodrigo <jorgerodrigo.monteiro19@gmail.com>
2024-05-22 09:16:39 -0300

    Entregando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+
+Nokia cel1 = new Nokia(numero: "15921556578",modelo:"Modelo teste",imei:"8484asda484", memoria: 456);
+cel1.Ligar();
+cel1.ReceberLigacao();
+cel1.InstalarAplicativo("pou");
+
+Console.WriteLine("Iphone:");
+Iphone cel2 = new Iphone(numero: "11935786321",modelo:"Modelo caro",imei:"5474sahghda256", memoria: 200);
+cel2.Ligar();
+cel2.ReceberLigacao();
+cel2.InstalarAplicativo("Subway surf");
